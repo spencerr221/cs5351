9c2f0bb632b4ccb999155d13f228b40a7967d58b
14329 <14329@175.159.69.119>
2021-09-13 20:33:07 +0800

    alter the code with a bug

--- junit5-maven/src/main/java/Samples/FloorSquareRoot.java
+++ junit5-maven/src/main/java/Samples/FloorSquareRoot.java
@@ -9,7 +9,7 @@ public class FloorSquareRoot {
             int mid = (start + end) / 2;
             if (mid * mid == x) return mid;
             if (mid * mid < x) {
-                start = mid + 1;
+                start = mid + 5;
                 ans = mid;
             } else {
                 end = mid - 1;
